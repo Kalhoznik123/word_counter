06e313d59d27c9de9f547024e9199374fa5f1c2b
Ivan <xx@ya.ru>
2023-11-23 16:44:07 +0300

    init_commit

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <sstream>
+#include <iterator>
+#include <cctype>
+
+class WordCounter
+{
+private:
+    std::istream& in_;
+
+
+
+
+public:
+    struct Data{
+        std::size_t chars{0};
+        std::size_t words{0};
+        std::size_t lines{0};
+    };
+
+    WordCounter(std::istream& in):in_(in) {}
+
+    Data count (){
+
+        std::size_t chars{0};
+        std::size_t words{0};
+        std::size_t lines{0};
+        std::string word;
+        char prev_char = ' ';
+
+        char ch  = in_.get();
+        while(!in_.eof()) {
+
+
+            if(ch == '\n'){
+                ++lines;
+            }
+
+            if(ch == ' '){
+
+            }else if(ch == '\n'){
+
+
+            }
+
+            if(std::isalnum(prev_char)&&(ch == '\n' || ch == ' '))
+                ++words;
+
+            prev_char = ch;
+            ++chars;
+            ch  = in_.get();
+        }
+        if(std::isalnum(prev_char)){
+            ++words;
+        }
+        return {chars,words,lines};
+
+    }
+
+};
+
+
+
+int main()
+{
+    std::istringstream str("aa aa\na a\n");
+
+    WordCounter wc(str);
+
+    auto data = wc.count();
+
+    std::cout << data.lines <<' '<< data.words <<' '<< data.chars << '\n';
+
+    return 0;
+}
